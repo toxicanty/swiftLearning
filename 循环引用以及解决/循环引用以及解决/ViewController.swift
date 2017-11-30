1dacb6abb35e8eaf4a2b672558d0d9fa94ef8c1b
toxicanty <donkey1@qq.com>
2017-11-30 13:34:34 +0800

    产生循环引用的一个例子// a demo of circular reference

---
+++ 循环引用以及解决/循环引用以及解决/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  循环引用以及解决
+//
+//  Created by toxicanty on 2017/11/30.
+//  Copyright © 2017年 toxicanty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // 声明变量
+    var cpCompletion :(()->())?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        loadData {
+            print(self.view)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    deinit {
+        print("I die")
+    }
+    
+    func loadData(completion:@escaping ()->()) -> () {
+        // 赋值给属性
+        cpCompletion = completion
+        
+        DispatchQueue.global().async {
+            completion()
+        }
+    }
+
+}
+

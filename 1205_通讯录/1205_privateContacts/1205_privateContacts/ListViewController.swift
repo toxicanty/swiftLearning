4875420cc525ffad31e6c1ca418be72053bbfa8d
toxicanty <donkey1@qq.com>
2017-12-05 13:37:46 +0800

    私人通讯录demo框架搭建

---
+++ 1205_通讯录/1205_privateContacts/1205_privateContacts/ListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ListViewController.swift
+//  1205_privateContacts
+//
+//  Created by toxicanty on 2017/12/5.
+//  Copyright © 2017年 toxicanty. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+   
+}
---
+++ 1205_通讯录/1205_privateContacts/1205_privateContactsTests/_205_privateContactsTests.swift
@@ -0,0 +1,36 @@
+//
+//  _205_privateContactsTests.swift
+//  1205_privateContactsTests
+//
+//  Created by toxicanty on 2017/12/5.
+//  Copyright © 2017年 toxicanty. All rights reserved.
+//
+
+import XCTest
+@testable import _205_privateContacts
+
+class _205_privateContactsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

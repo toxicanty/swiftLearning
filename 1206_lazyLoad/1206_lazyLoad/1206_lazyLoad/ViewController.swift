0cb4c43ab0972d9b7ffe16d5f87a66b2dd9e7b40
toxicanty <donkey1@qq.com>
2017-12-06 15:28:20 +0800

    懒加载本质上是闭包, 可能的变式写法, 详细见代码中的注释:进化3

--- 1206_lazyLoad/1206_lazyLoad/1206_lazyLoad/ViewController.swift
+++ 1206_lazyLoad/1206_lazyLoad/1206_lazyLoad/ViewController.swift
@@ -17,10 +17,18 @@ class ViewController: UIViewController {
     //进化2: 这种懒加载方式,用到label时候才去实例化, 而且使用时的问号,惊叹号都没有了.
     lazy var label:DemoLabel = DemoLabel()
     
+    // 进化3: 不建议这么写,但是要认识(swift4已经不支持了,或者变化了)
+    // 不建议的原因:1闭包中的智能提示不好,2闭包中如果出现self. 需要注意循环引用
+    // 懒加载本质上是闭包,完整写法如下: {}包装的代码, ()执行代码
+//    lazy var label = {()->DemoLabel in
+//        let  l = DemoLabel()
+//        // 这里可以设置label的属性
+//        return l
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         setupUI()
     }
 

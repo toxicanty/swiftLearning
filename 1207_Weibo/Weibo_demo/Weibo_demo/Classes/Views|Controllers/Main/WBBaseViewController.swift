c187a031fe464e8fcbbe52e7be433b215a5b8386
toxicanty <donkey1@qq.com>
2017-12-14 17:30:42 +0800

    重写navigationbar.navigationItem.title的didSet方法...

--- 1207_Weibo/Weibo_demo/Weibo_demo/Classes/Views|Controllers/Main/WBBaseViewController.swift
+++ 1207_Weibo/Weibo_demo/Weibo_demo/Classes/Views|Controllers/Main/WBBaseViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class WBBaseViewController: UIViewController {
+    
+    // 自定义导航条
+    lazy var navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: UIScreen.yw_screenWidth(), height: 64))
 
+    // 自定义导航条目
+    lazy var navItem = UINavigationItem()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,12 +23,18 @@ class WBBaseViewController: UIViewController {
         setupUI()
     }
     
+    override var title: String?{
+        didSet{
+            navItem.title = title
+        }
+    }
     
    func setupUI(){
         view.backgroundColor = UIColor.yw_random()
     
-        
+        // 添加导航条
+    view.addSubview(navigationBar)
+    navigationBar.items = [navItem]
     }
 }
-
 
--- 1207_Weibo/Weibo_demo/Weibo_demo/Classes/Views|Controllers/Main/WBNavigationController.swift
+++ 1207_Weibo/Weibo_demo/Weibo_demo/Classes/Views|Controllers/Main/WBNavigationController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class WBNavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        
+        // 隐藏系统的navigationBar
+        navigationBar.isHidden = true //一种方法...不能push了,整个上面都没有了.
+    }
  
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         
